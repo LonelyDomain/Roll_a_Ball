04b1a724b6fec58758e7802bd956fccf24764314
LonelyDomain <juderage@hotmail.com>
2016-07-13 13:47:12 -0500

    Initial Commit
    Completed Tutorial

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start ()
+	{
+		transform.position = new Vector3(-2, 4, 0);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		//Player to move left right up and down
+		//Player (gameobject) aka transform to move when arrow keys are pressed
+
+		transform.Translate(Vector3.right * Input.GetAxis("Horizontal"));
+		transform.Translate(Vector3.forward * Input.GetAxis("Vertical"));
+		transform.Translate(Vector3.up * Input.GetAxis("Jump"));
+	}
+
+
+}
+       
---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Player_Controller : MonoBehaviour 
+{
+
+
+
+	private Rigidbody rb;
+	public float speed;
+	public Text countText;
+	public Text winText; 
+	public float jumpModifier;
+	private int count;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float jumpForce = Input.GetAxis ("Jump"); 
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal* speed, jumpForce* jumpModifier, moveVertical* speed);
+
+		rb.AddForce (movement);
+	}
+  
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText (); 
+		}
+	}
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 4) {
+			winText.text = "You Win";
+		}
+	}
+}
